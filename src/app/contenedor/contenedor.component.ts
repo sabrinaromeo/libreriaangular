1c929131188e50806ca69f68c921e68f84bac8dd
Marco Faccio <marco.faccio@gmail.com>
2020-08-25 09:22:00 -0300

    Se ajustan comentarios de metodos por ensima de la funcion y no a un costado y se hacen ajusten menores en la sintaxis.

--- src/app/contenedor/contenedor.component.ts
+++ src/app/contenedor/contenedor.component.ts
@@ -1,32 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { LibrosService } from '../libros.service';
+import {Component, OnInit} from '@angular/core';
+import {LibrosService} from '../libros.service';
 
-@Component({
-  selector: 'app-contenedor',
-  templateUrl: './contenedor.component.html',
-  styleUrls: ['./contenedor.component.css']
-})
-export class ContenedorComponent implements OnInit {
+@Component
+  ({
+    selector: 'app-contenedor',
+    templateUrl: './contenedor.component.html',
+    styleUrls: ['./contenedor.component.css']
+  })
+export class ContenedorComponent implements OnInit
+{
+  lista: any = [];
 
+  constructor(private LibrosService: LibrosService) {}/*aca hago la inyeccion de dependencia DEL SERVICE LIBROS (libros.service.ts) el prime librosService es el nombre que va a tener en el contendeor y el segundo es el nombre que tiene el service ACA DECIMOS QUE VAMOS A USAR EL SERVICE LIBROS*/
 
-  lista:any=[];
+  async ngOnInit()
+  {
 
-  constructor( private LibrosService:LibrosService ) { }/*aca hago la inyeccion de dependencia DEL SERVICE LIBROS (libros.service.ts) el prime librosService es el nombre que va a tener en el contendeor y el segundo es el nombre que tiene el service ACA DECIMOS QUE VAMOS A USAR EL SERVICE LIBROS*/
-
-   async ngOnInit() {
-
-    this.lista= await this.LibrosService.listaDeLibros();
+    this.lista = await this.LibrosService.listaDeLibros();
     /* this.lista es el array declarado mas arriba, y estoy entrando e libros service a la funcion listade libros que es la que BUSCA TODOS MIS LIBROS EN EL SERVIDOR.
     COMO TENGO QUE ESPERAR A QUE EL SERVIDOR REPOSNDA ESTE METODO ES ASINCRONICO*/
   }
 
-  async agregarLibroABiblioteca(){ /*esta es la funcion que se comunica con el emitter de nuevolibro.ts y que me manda  ese NUEVO LIBRO  */
-    this.lista= await this.LibrosService.listaDeLibros(); //vuelvo a pedir la lista de libros ya que se actualiza con el nuevo libro ingrsado
-
-    
-    //this.lista.push(nuevoLibro);agrego el libro a la lista que va a recibir  la LIBRERIA.TS es donde se mostraran los libros que cargue
-    //console.log(nuevoLibro);veo el libro por consola
-
+  /**
+   * Se comunica con el emitter de nuevolibro.ts y que me manda  ese NUEVO LIBRO
+   */
+  async agregarLibroABiblioteca()
+  {
+    this.lista = await this.LibrosService.listaDeLibros(); //vuelvo a pedir la lista de libros ya que se actualiza con el nuevo libro ingrsado
   }
 
 }
